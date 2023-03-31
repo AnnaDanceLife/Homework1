44d73c7a7a621ea25101a5c609d3c536c825225d
Anna Shatilova <anxet@mail.ru>
2023-03-31 10:11:40 +0300

    Изменение названий переменных

--- hw-1.js
+++ hw-1.js
@@ -11,40 +11,40 @@ alert(javaScriptCreator);
 
 let c = 10;
 let d = 2;
-alert (c+d);
-alert (c-d);
-alert (c*d);
-alert (c/d);
+alert(c + d);
+alert(c - d);
+alert(c * d);
+alert(c / d);
 
-let result = 2**5;
-alert (result);
+let result = 2 ** 5;
+alert(result);
 
 let e = 9;
 let f = 2;
-alert (e%f);
+alert(e % f);
 
 let num = 1;
 num += 5;
 num -= 3;
 num *= 7;
 num /= 3;
-num ++;
-num --;
+num++;
+num--;
 alert(num);
 
-let age = prompt ('Сколько вам лет?');
-alert (age);
+let age = prompt('Сколько вам лет?');
+alert(age);
 
 let user = {
     name: 'Anna',
     age: 35,
     isAdmin: true
 }
-user ['city of residence'] = true;
+user['city of residence'] = true;
 user.age = 40;
-delete user ['city of residence'];
+delete user['city of residence'];
 let info = prompt('Какую информацию хотите узнать о пользователе?');
-alert (user[info]);
+alert(user[info]);
 
 let userName = prompt('Как Вас зовут?');
 alert(`Привет, ${userName}!`);
